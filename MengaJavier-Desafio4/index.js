dd3660e6cc53b78e63bd24cd3bddc0e59e6c5d39
javier.menga <jfm94@hotmail.com>
2022-04-13 16:38:47 -0300

    Desafío4 modificado - genere toda la lógica para chequear en POSTMAN y el html con el formulario

--- MengaJavier-Desafio4/index.js
+++ MengaJavier-Desafio4/index.js
@@ -1,20 +1,154 @@
+// FS
+const fs = require('fs')
+
 // Express
 const express = require('express')
 const app = express()
-    // const { Router } = express
-    // const router = Router()
+const { Router } = express
+const router = Router()
+const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
 
 // Codigo para que la app use los router
-// app.use('/holi', router)
+app.use('/api/productos', router)
+
+// Codigo para que levante el html
+app.use('static', express.static(__dirname + 'public'))
+
+// Codigo para que interprete mejor los json que envía el cliente?
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // Server
 const server = app.listen(8080, () => { console.log('Server is running on port 8080') })
 server.on('error', (err) => { console.log(err) })
 
+// Defino mi clase "Container" de Productos con sus respectivos metodos
+class Container {
+    constructor(fileName) {
+        this.fileName = fileName;
+    }
+
+    save(object) {
+        if (!fs.existsSync(this.fileName)) {
+            object.id = 1;
+            let json = JSON.stringify([object]);
+            fs.writeFileSync(this.fileName, json);
+            console.log(object.id)
+        } else {
+            let json = fs.readFileSync(this.fileName, 'utf-8');
+            let data = JSON.parse(json);
+            if (data.length > 0) {
+                object.id = data[data.length - 1].id + 1;
+                data.push(object);
+                let json2 = JSON.stringify(data);
+                fs.writeFileSync(this.fileName, json2);
+                console.log(object.id)
+            } else {
+                object.id = 1;
+                let json = JSON.stringify([object]);
+                fs.writeFileSync(this.fileName, json);
+                console.log(object.id)
+            }
+        }
+
+    }
+
+    getById(number) {
+        if (fs.existsSync(this.fileName)) {
+            let json = fs.readFileSync(this.fileName, 'utf-8');
+            let data = JSON.parse(json);
+            if (data.length > 0) {
+                let productById = data.find(x => x.id === number);
+                return productById
+                    // console.log(productById)
+            } else {
+                console.log('Null')
+            }
+        }
+    }
+
+    getAll() {
+        let json = fs.readFileSync(this.fileName);
+        let object = JSON.parse(json);
+        return object
+    }
+
+    deleteById(number) {
+        let json = fs.readFileSync(this.fileName);
+        let object = JSON.parse(json);
+        object.splice([number - 1], 1);
+        let json2 = JSON.stringify(object);
+        fs.writeFileSync(this.fileName, json2);
+    }
+
+    deleteAll() {
+        let json = fs.readFileSync(this.fileName);
+        let object = JSON.parse(json);
+        object = [];
+        let json2 = JSON.stringify(object);
+        fs.writeFileSync(this.fileName, json2);
+    }
+
+    modifyById(number, object) {
+        let json = fs.readFileSync(this.fileName, 'utf-8');
+        let data = JSON.parse(json);
+        data[number - 1] = object;
+        let json2 = JSON.stringify(data);
+        fs.writeFileSync(this.fileName, json2);
+
+    }
+}
+// Defino mi clase "Product" con los metodos de la clase "Container"
+let Products = new Container('productos.txt');
+
+
+
 // Rutas
-app.get('/', (req, res) => {
-        res.send('Hola')
-    })
-    // router.get('/', (req, res) => {
-    //     res.send('Holi2')
-    // })
+// ruta para obtener todos los productos
+router.get('/', (req, res) => {
+    let allProducts = Products.getAll();
+    res.json(allProducts);
+})
+
+// ruta para obtener un producto según id
+router.get('/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    let foundProduct = Products.getById(id)
+    if (foundProduct === undefined) {
+        res.send({ error: 'Producto no encontrado' })
+    } else {
+        res.json(foundProduct)
+    }
+})
+
+// ruta para agregar un producto según la información que se envía
+router.post('/', jsonParser, (req, res) => {
+    let newProduct = req.body;
+    Products.save(newProduct);
+    res.json(newProduct);
+})
+
+// ruta para modificar un producto según id y la información que se envía
+router.put('/:id', jsonParser, (req, res) => {
+    let id = parseInt(req.params.id);
+    let productModified = req.body;
+    let foundProduct = Products.getById(id);
+    if (foundProduct === undefined) {
+        res.send({ error: 'Producto no encontrado' })
+    } else {
+        (foundProduct).title = productModified.title;
+        (foundProduct).price = productModified.price;
+        (foundProduct).thumbnail = productModified.thumbnail;
+        Products.modifyById(id, foundProduct);
+        res.json(foundProduct);
+    }
+})
+
+// ruta para borrar un producto según id
+router.delete('/:id', (req, res) => {
+    let id = parseInt(req.params.id);
+    Products.deleteById(id);
+    res.send('Producto eliminado')
+})
